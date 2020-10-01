a872ba0c5232c2cede1974a0c7e2e5f41e8b1bdd
Jennifer Ruan <jennifer.ruan@gmail.com>
2020-09-30 22:07:32 -0400

    Added test that checks if the app renders

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+
+test('renders app', () => {
+  const { getByText } = render(<App />);
+  const title = getByText('Groceries');
+  expect(title).toBeInTheDocument();
+});
