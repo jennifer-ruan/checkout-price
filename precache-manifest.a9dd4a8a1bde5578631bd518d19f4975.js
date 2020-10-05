ade656c29391d6ed32b53023a67ece98d6ed88a6
Jennifer Ruan <jennifer.ruan@gmail.com>
2020-10-05 16:51:58 -0400

    Updates

---
+++ precache-manifest.a9dd4a8a1bde5578631bd518d19f4975.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a4e736b45316c4df2af26a48c909d3",
+    "url": "/checkout-price/index.html"
+  },
+  {
+    "revision": "520dc094b4794b74ca9c",
+    "url": "/checkout-price/static/css/main.18a6e4c2.chunk.css"
+  },
+  {
+    "revision": "0ba00ce39353b7722cdc",
+    "url": "/checkout-price/static/js/2.84482ce9.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/checkout-price/static/js/2.84482ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520dc094b4794b74ca9c",
+    "url": "/checkout-price/static/js/main.64f5c266.chunk.js"
+  },
+  {
+    "revision": "8d070ddb18927272779f",
+    "url": "/checkout-price/static/js/runtime-main.4991ff37.js"
+  },
+  {
+    "revision": "656f230714b9da0f84cdfc48d5aeddf6",
+    "url": "/checkout-price/static/media/ground_beef.656f2307.jpg"
+  },
+  {
+    "revision": "57bf3bc048fc81900437c18ec656ff79",
+    "url": "/checkout-price/static/media/lettuce.57bf3bc0.jpg"
+  },
+  {
+    "revision": "f63ead8467ffae290e9c3ffcdc778c5e",
+    "url": "/checkout-price/static/media/squid.f63ead84.jpg"
+  }
+]);
